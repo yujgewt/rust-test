7f1e9c8edf9ffe1fb3be0d0b2ddf7e96dde7ed08
Jaegwan Yu <compaso@naver.com>
2021-11-24 08:30:47 +0000

    unit test 추가, 모듈화

--- src/utils/utils.rs
+++ src/utils/utils.rs
@@ -1,9 +1,11 @@
+mod utils;
 
-
-pub fn KeyExpansion(key:u32){
+pub fn util_api(key:u32){
     println!("{:?}",key);
-
+    key
     //let mut w:[[u8;4];4];
 
 
 }
+
+
--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,4 @@
-
-
+use rusttest::*;
 
 #[cfg(test)]
 mod tests {
@@ -7,11 +6,77 @@ mod tests {
 
 
     #[test]
-    fn test_G() {
-        println!("test started.");
-        println!("{:0X}",KeyExpansion(1));
+    fn test_KeyExpansion() {
         
+        let mut key:[u32;4] = [0x2b7e1516,0x28aed2a6,0xabf71588,0x09cf4f3c];
+        
+        for i in 0..10{
+
+            key  = rusttest::KeyExpansion(key,i);
+            println!("{:0X}",key[0]);
+            println!("{:0X}",key[1]);
+            println!("{:0X}",key[2]);
+            println!("{:0X}\n",key[3]);
+
+        }
         
     }
 
+
+
+    #[test]
+    fn test_encryption(){
+
+        //let mut key:[u32;4] = [0x2b7e1516,0x28aed2a6,0xabf71588,0x09cf4f3c];
+        let mut key:[u8;16] =  [
+                                0x2b,0x7e,0x15,0x16,
+                                0x28,0xae,0xd2,0xa6,
+                                0xab,0xf7,0x15,0x88,
+                                0x09,0xcf,0x4f,0x3c
+                                ];
+        
+        //SubBytes(mut a:u8)
+
+        
+
+
+
+        
+    }
+
+
+    #[test]
+    fn test_ShiftRows(){
+
+        //let mut key:[u32;4] = [0x2b7e1516,0x28aed2a6,0xabf71588,0x09cf4f3c];
+        let mut key:[u8;16] =  [
+                                0x2b,0x7e,0x15,0x16,
+                                0x28,0xae,0xd2,0xa6,
+                                0xab,0xf7,0x15,0x88,
+                                0x09,0xcf,0x4f,0x3c
+                                ];
+        
+        println!("{:?}",rusttest::ShiftRows(key));
+        
+    }
+
+
+    #[test]
+    fn test_Mixcolumns(){
+
+        //let mut key:[u32;4] = [0x2b7e1516,0x28aed2a6,0xabf71588,0x09cf4f3c];
+        let mut key:[u8;16] =  [
+                                0x2b,0x7e,0x15,0x16,
+                                0x28,0xae,0xd2,0xa6,
+                                0xab,0xf7,0x15,0x88,
+                                0x09,0xcf,0x4f,0x3c
+                                ];
+        
+        println!("{:?}",rusttest::MixColumns(key));
+        
+    }
+
+
+
 }
+
